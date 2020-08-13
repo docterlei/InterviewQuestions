fdf1c282cacb58e22252c33f5b14e835dc203c0e
leilei <1101668549@qq.com>
2020-08-13 21:14:17 +0800

    feat: selectionSort

--- sort/simpleSelectionSort.js
+++ sort/simpleSelectionSort.js
@@ -2,11 +2,11 @@
  * @Author: Guohui Lei 
  * @Date: 2020-08-13 20:40:06 
  * @Last Modified by: Guohui Lei
- * @Last Modified time: 2020-08-13 21:05:10
+ * @Last Modified time: 2020-08-13 21:14:12
  */
 
 
- // 简单选择排序算法
+ // 简单选择排序算法 原理是 每轮比较找出最小值的索引 然后默认索引对比 如果不相等 就交换
  
  var arr = [9,1,5,8,3,7,4,6,2,0];
  const selectSort = arr => {
