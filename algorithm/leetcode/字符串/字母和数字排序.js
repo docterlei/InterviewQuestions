81172744bd6e558e464e9ba4fef2298b8a10abe5
leilei <1101668549@qq.com>
2021-03-08 21:51:19 +0800

    feat: function

--- algorithm/leetcode/字符串/字母和数字排序.js
+++ algorithm/leetcode/字符串/字母和数字排序.js
@@ -1,14 +1,17 @@
 // let value = ["C1", "C2", "B1", "A1", "A3"]
-let value = ["A1", "C2", "B1", "C1", "A3"]
+let value = ["A1", "C2", "B1", "C1", "A3"];
 function sortFun(a, b) {
-    let c1 = a[0], c2 = b[0], num1 = a[1], num2 = b[1];
-    if(c1 != c2) {
-        if(c1 < c2) return 1;
-        return -1;
-    }
-    return num1 -num2;
+  let c1 = a[0],
+    c2 = b[0],
+    num1 = a[1],
+    num2 = b[1];
+  if (c1 != c2) {
+    if (c1 < c2) return 1;
+    return -1;
+  }
+  return num1 - num2;
 }
 
-value.sort(sortFun)
+value.sort(sortFun);
 
-console.log(value)
+console.log(value);
